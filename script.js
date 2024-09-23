4b854ea3f5788608337840d9d9857dd66f768ded
geilsonfreire <geilsonfreireleite@gmail.com>
2024-09-23 18:45:43 -0300

    Refatorar estilos globais e responsividade

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+// Declarand variables
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
+// aplicando eventos de click nos botões
 sign_up_btn.addEventListener("click", () => {
   container.classList.add("sign-up-mode");
-});
+}); // adiciona a classe sign-up-mode ao container
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
-});
+}); // remove a classe sign-up-mode do container
